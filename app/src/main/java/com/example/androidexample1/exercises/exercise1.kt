dab28be212845239f3afc62bebdedfaaf26396aa
aylinbq <quinosarai@gmail.com>
2024-01-02 15:01:58 -0400

    Adding with patch

--- app/src/main/java/com/example/androidexample1/exercises/exercise1.kt
+++ app/src/main/java/com/example/androidexample1/exercises/exercise1.kt
@@ -1,14 +1,12 @@
 package com.example.androidexample1.exercises
 /**
- * Ejemplo Pagina Web
- * Notigficaciones
- * 
  * Por lo general, el teléfono te proporciona un resumen de las notificaciones.
  * En el código inicial que se proporciona en el siguiente fragmento de código, escribe un programa que imprima el mensaje de resumen según la cantidad de notificaciones que recibiste. El mensaje debe incluir lo siguiente:
  *
  * La cantidad exacta de notificaciones cuando haya menos de 100
  * 99+ como cantidad de notificaciones cuando haya 100 o más
  *
+ * Examples of Android
  * */
 fun main() {
     val morningNotification = 51
@@ -22,3 +20,7 @@ fun printNotificationSummary(numberOfMessages: Int) {
     if (numberOfMessages<100) println("You have $numberOfMessages notifications.")
     else println("Your phone is blowing up! You have 99+ notifications.")
 }
+
+/**
+ * Resuelto
+ * */
